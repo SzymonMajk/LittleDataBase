c98bda3b4331c63483b026bfa4da16b804c72fb3
Szymon Majkut <majkszym@borg.kis.agh.edu.pl>
2018-01-08 19:59:24 +0100

    a to szkoda

--- lab11/ex_1.sql
+++ lab11/ex_1.sql
@@ -1,13 +1,13 @@
 --todo
 create or replace function masaPudelka(nr char(4))
-returns numeric as
+returns numeric(7,2) as
 $$
 declare
 	wiersz record;
-	suma numeric;
+	suma numeric(7,2);
 begin
-	for wiersz in select (masa,sztuk) into suma
-		from czekoladki natural join zawartosc
+	for wiersz in select (c.masa*z.sztuk) into suma
+		from czekoladki c natural join zawartosc z
 		where idpudelka = nr
 	loop
 		suma := suma + masa*sztuk;
